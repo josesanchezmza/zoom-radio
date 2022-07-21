8cf924317c466f8318baae733d8393274c5e5a3e
jsanchez <josesanchezmza@gmail.com>
2022-07-21 16:11:56 -0300

    mejores en boton play

--- src/app/components/play-button/play-button.component.ts
+++ src/app/components/play-button/play-button.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-play-button',
   templateUrl: './play-button.component.html',
   styleUrls: ['./play-button.component.scss'],
 })
-export class PlayButtonComponent implements OnInit {
-
-  isStreaming = true;
+export class PlayButtonComponent {
+  @Input() isStreaming: boolean;
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {}
+  onClick() {
+    this.btnClick.emit();
+  }
 
-
+  setIcon(streaming){
+    if(streaming){
+      return 'radio';
+    } else {
+      return 'play';
+    }
+  }
 }
