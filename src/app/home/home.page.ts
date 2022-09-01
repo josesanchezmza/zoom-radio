deee5882f27d78301bd8a735fe6645533072aaa1
Jose Sanchez <josesanchezmza@gmail.com>
2022-09-01 19:38:09 -0300

    creacion de service para consumir api de data de radio. refactor de global scss

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,13 +2,16 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { RangeValue } from '@ionic/core';
 import { RangeCustomEvent } from '@ionic/angular';
+import { DataService } from '../services/data.service';
+import { interval } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   @ViewChild('player') player: ElementRef;
   isStreaming = false;
   isMuted = false;
@@ -16,8 +19,14 @@ export class HomePage {
   telegramURL = environment.telegramURL;
   phoneNumber = environment.phoneNumber;
   lastEmittedValue: RangeValue;
+  listeners: any;
 
-  constructor() {}
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.getRadioInfo();
+    this.updateRadioInfo(2 * 60 * 1000);
+  }
 
   playRadio() {
     this.isStreaming = !this.isStreaming;
@@ -45,5 +54,25 @@ export class HomePage {
     } else {
       return 'volume-medium-outline';
     }
+  }
+
+  updateRadioInfo(milliseconds: number) {
+    setInterval(() => {
+      this.getRadioInfo();
+      console.log('setInterval');
+    }, milliseconds);
+  }
+  getRadioInfo() {
+    // interval(1 * 60 * 1000)
+    //   .pipe(mergeMap(() => this.dataService.getRadioInfo()))
+    //   .subscribe(({ data }) => {
+    //     this.listeners = data[0].listeners;
+    //     console.log('listeners: ', this.listeners);
+    //   });
+
+    this.dataService.getRadioInfo().subscribe(({ data }) => {
+      this.listeners = data[0].listeners;
+      console.log('getRadioInfo: ', this.listeners);
+    });
   }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,59 @@
+export interface RadioInfo {
+  type: string;
+  data: Datum[];
+}
+
+export interface Datum {
+  title: string;
+  song: string;
+  track: Track;
+  bitrate: string;
+  server: string;
+  autodj: string;
+  source: string;
+  offline: boolean;
+  summary: string;
+  listeners: number;
+  maxlisteners: number;
+  reseller: number;
+  serverstate: boolean;
+  sourcestate: boolean;
+  sourceconn: number;
+  date: string;
+  time: string;
+  rawmeta: string;
+  mountpoint: string;
+  tuneinurl: string;
+  directtuneinurl: string;
+  proxytuneinurl: string;
+  tuneinformat: string;
+  webplayer: string;
+  servertype: string;
+  listenertotal: number;
+  url: string;
+}
+
+export interface Track {
+  title: string;
+  artist: string;
+  album: string;
+  imageurl: string;
+}
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  radioInfoUrl = environment.radioInfo;
+  radioData: RadioInfo;
+
+  constructor(private http: HttpClient) {}
+
+  getRadioInfo() {
+    return this.http.get<RadioInfo>(this.radioInfoUrl);
+  }
+}
