3c3c6c3ef709ea51d777803942ea1354d6402611
Jose Sanchez <josesanchezmza@gmail.com>
2022-09-08 16:41:05 -0300

    actualizacion de url de audio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   radioURL: 'https://radio01.ferozo.com/proxy/ra01000750?mp=/stream',
-  autoDjURL: 'https://radio01.ferozo.com/proxy/ra01000750?mp=/autodj',
+  autodjURL: 'https://radio01.ferozo.com/proxy/ra01000750?mp=/autodj',
   radioInfo: 'https://radio01.ferozo.com:2199/rpc/ra01000750/streaminfo.get',
   telegramURL: 'https://t.me/ZOOMRADIOMLG',
   phoneNumber: '+54 9 260 423 8630',
